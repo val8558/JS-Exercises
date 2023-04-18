9079a94f3e795f5bb9b67d5b43ba1d3ccc51f869
val8558 <valmmir@hotmail.com>
2023-04-18 00:08:19 -0300

    exercicios

---
+++ sistema_notas.js
@@ -0,0 +1,40 @@
+function randomScore(min, max) {
+    min = Math.ceil(0)
+    max = Math.floor(100)
+    return Math.floor(Math.random() * (max - min))
+}
+let score = randomScore(0, 100)
+
+
+function getScore() {
+    
+    
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 89 && score <= 89
+    let scoreD = score >= 60 && score <= 69
+    let scoreC = score >= 70 && score <= 79
+    let scoreF = score < 60 && score >= 0
+
+    let scoreFinal;
+
+    if (scoreA){
+        scoreFinal = "A";}
+
+    else if(scoreB){
+        scoreFinal = "B";} 
+       
+    else if(scoreC){
+        scoreFinal = "C";}
+    
+    else if(scoreD){
+        scoreFinal = "D";}
+
+    else if(scoreF){
+        scoreFinal = "F";
+    }  
+    else {scoreFinal = "nota invalida"}
+    
+    return scoreFinal
+}
+
+console.log(getScore(score))
